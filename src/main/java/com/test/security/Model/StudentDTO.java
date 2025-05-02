41ddd01fc6e4d09aae20b1e575c14133965d34d6
m.ammar.ext <muhammad4ammar23@gmail.com>
2025-05-02 14:50:45 +0300

    "connect with db "

---
+++ src/main/java/com/test/security/Model/StudentDTO.java
@@ -0,0 +1,21 @@
+package com.test.security.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDTO {
+
+
+    //properties
+    private Integer rollNo;
+    private String name;
+    private Integer mark;
+
+
+}
---
+++ src/main/java/com/test/security/Repository/StudentRepo.java
@@ -0,0 +1,10 @@
+package com.test.security.Repository;
+
+import com.test.security.Entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student,Integer> {
+
+}
